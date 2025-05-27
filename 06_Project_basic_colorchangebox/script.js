79f0dcf5f901f62a861e3f683641e9533a850b96
Bhupendra <errorbhupendra481@gmail.com>
2025-05-27 22:27:15 +0530

    added a basic project of color_change_background

---
+++ 06_Project_basic_colorchangebox/script.js
@@ -0,0 +1,31 @@
+//by click to the particular box, the color of background will change
+
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+button.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click', function(event) {
+    console.log(event.target);
+    if(event.target.id === 'grey') {
+        body.style.backgroundColor = 'grey';
+    }
+    if(event.target.id === 'white') {
+        body.style.backgroundColor = 'white';
+    }
+    if(event.target.id === 'blue') {
+        body.style.backgroundColor = 'blue';
+    }
+    if(event.target.id === 'yellow') {
+        body.style.backgroundColor = 'yellow';
+    }
+});
+});
+
+
+
+
+
+
+
+
